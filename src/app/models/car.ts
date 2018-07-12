b898e2397984dba0551b8def17e5bff9f4e8bd31
Carlos Martin <cjmar87@gmail.com>
2018-07-12 01:38:12 -0500

    modelos y menu

---
+++ src/app/models/car.ts
@@ -0,0 +1,11 @@
+export class Car(){
+  constructor(
+    id:number,
+    title:string,
+    description:string,
+    price:number,
+    status:string,
+    created_at,
+    updated_at
+  ){}
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,9 @@
+export class User{
+  constructor(
+    id:number,
+    role:string,
+    name:string,
+    email:string,
+    password:string
+  ){}
+}
