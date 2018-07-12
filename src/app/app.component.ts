29275c4f1faccd0b56bc512c143b53dbf34a3599
Carlos Martin <cjmar87@gmail.com>
2018-07-12 18:10:34 -0500

    empezamos el detalle de los objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { UserService } from './services/user.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [UserService];
+  providers: [UserService]
 })
 export class AppComponent implements OnInit, DoCheck {
   title = 'app';
--- src/app/components/default/default.component.ts
+++ src/app/components/default/default.component.ts
@@ -4,24 +4,41 @@ import {Router, ActivatedRoute, Params} from '@angular/router';
 import { User } from '../../models/user';
 import { UserService } from '../../services/user.service';
 
+import { Car } from '../../models/car';
+import { CarService } from '../../services/car.service';
+
 @Component({
   selector: 'default',
   templateUrl: './default.component.html',
-  providers: [UserService]
+  providers: [UserService,CarService]
 })
 
 export class DefaultComponent implements OnInit{
   title:string;
+  cars: Array<Car>;
 
   constructor(
     private _route: ActivatedRoute,
     private _router: Router,
-    private _userService: UserService
+    private _userService: UserService,
+    private _carService: CarService
   ){
     this.title = 'Inicio';
   }
 
   ngOnInit(){
     console.log('Default component cargado correctamente');
+    this._carService.getCars().subscribe(
+      response => {
+        if(response.status == 'success'){
+          this.cars = response.cars;
+        }else{
+          
+        }
+      },
+      error => {
+        console.log(<any>error)
+      }
+    );
   }
 }
